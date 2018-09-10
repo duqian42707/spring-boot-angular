3922551b44fb5a9d6550ddf13aa2718f69eae525
duqian <duqian42707@163.com>
2018-09-10 21:25:10 +0800

    tmp-

--- front-end/soccer-web/src/app/routes/sys/role/edit/edit.component.ts
+++ front-end/soccer-web/src/app/routes/sys/role/edit/edit.component.ts
@@ -1,15 +1,17 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {NzModalRef, NzMessageService} from 'ng-zorro-antd';
+import {NzModalRef, NzMessageService, NzTreeComponent} from 'ng-zorro-antd';
 import {_HttpClient} from '@delon/theme';
 import {SFSchema, SFUISchema} from '@delon/form';
 import {FormBuilder, Validators} from "@angular/forms";
 import {RoleService} from "@core/service/sys/role.service";
+import {ModuleService} from "@core/service/sys/module.service";
 
 @Component({
   selector: 'app-sys-role-edit',
   templateUrl: './edit.component.html',
 })
 export class SysRoleEditComponent implements OnInit {
+  @ViewChild('nzTree') nzTree: NzTreeComponent;
   record: any = {};
   i: any;
   form;
@@ -19,6 +21,7 @@ export class SysRoleEditComponent implements OnInit {
     private modal: NzModalRef,
     public msgSrv: NzMessageService,
     public roleService: RoleService,
+    public moduleService: ModuleService,
     private fb: FormBuilder
   ) {
   }
@@ -26,7 +29,12 @@ export class SysRoleEditComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.fb.group({
       roleName: [null, [Validators.required]],
+      modules: [null, [Validators.required]],
     });
+    this.moduleService.getMenuForTreeNode().subscribe(res => {
+      this.form.patchValue({modules: res})
+    })
+
     if (this.record.id > 0)
       this.roleService.get(this.record.id).subscribe(res => {
         this.i = res.data;
@@ -39,10 +47,13 @@ export class SysRoleEditComponent implements OnInit {
     if (this.record.id > 0) {
       value.id = this.record.id;
     }
-    this.roleService.save(value).subscribe(res => {
-      this.msgSrv.success('保存成功');
-      this.modal.close(true);
-    });
+    var checkedNodeList = this.nzTree.getCheckedNodeList();
+    console.log(value)
+    console.log(checkedNodeList)
+    // this.roleService.save(value).subscribe(res => {
+    //   this.msgSrv.success('保存成功');
+    //   this.modal.close(true);
+    // });
   }
 
   close() {
