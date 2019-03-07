caca2834735a392d75593040a67ea47f00c9fc8f
duqian <duqian42707@163.com>
2019-03-07 15:14:13 +0800

    更新sql文件，加入初始化数据

--- back-end/soccer/deploy.sh
+++
@@ -1,7 +0,0 @@
-# /bin/sh
-
-docker rmi registry.cn-hangzhou.aliyuncs.com/duqian/soccer-back;
-
-mvn clean package -Dmaven.test.skip=true -P prod dockerfile:build \
- && docker push registry.cn-hangzhou.aliyuncs.com/duqian/soccer-back \
- && ssh aliyun "/root/soccer/update-backend.sh"
--- back-end/soccer/soccer.sql
+++ back-end/soccer/soccer.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : dqv5.com
+ Source Server         : 192.168.102.171
  Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : dqv5.com:3306
+ Source Server Version : 50556
+ Source Host           : 192.168.102.171:3306
  Source Schema         : soccer
 
  Target Server Type    : MySQL
- Target Server Version : 50721
+ Target Server Version : 50556
  File Encoding         : 65001
 
- Date: 12/09/2018 20:39:49
+ Date: 07/03/2019 15:12:46
 */
 
 SET NAMES utf8mb4;
@@ -52,9 +52,15 @@ CREATE TABLE `basic_user` (
   `last_modified_time` datetime DEFAULT NULL,
   `mod_user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKc4e23ymy8js3t97mv71idjjhh` (`mod_user_id`),
-  CONSTRAINT `FKc4e23ymy8js3t97mv71idjjhh` FOREIGN KEY (`mod_user_id`) REFERENCES `basic_user` (`id`)
+  KEY `FKc4e23ymy8js3t97mv71idjjhh` (`mod_user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Records of basic_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `basic_user` VALUES (1, 'admin', NULL, NULL, NULL, NULL, '$2a$10$dFfdqjrar3ri3TLANHP3SeMMUjNTO8KlXg67kDT9aeFIX1doPHLk6', NULL, NULL, '管理员', NULL, NULL);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for basic_user_role
@@ -65,7 +71,15 @@ CREATE TABLE `basic_user_role` (
   `user_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Records of basic_user_role
+-- ----------------------------
+BEGIN;
+INSERT INTO `basic_user_role` VALUES (1, 1, 1);
+INSERT INTO `basic_user_role` VALUES (2, 1, 2);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for sys_dict
@@ -97,8 +111,7 @@ CREATE TABLE `sys_func_right` (
   `perm_value` int(11) DEFAULT NULL,
   `module_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKojjk9tkjwsg2mb7isqvlnqot2` (`module_id`),
-  CONSTRAINT `FKojjk9tkjwsg2mb7isqvlnqot2` FOREIGN KEY (`module_id`) REFERENCES `sys_module` (`id`)
+  KEY `FKojjk9tkjwsg2mb7isqvlnqot2` (`module_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
@@ -112,8 +125,7 @@ CREATE TABLE `sys_log` (
   `operate_time` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK4xc8y0nxiniot9q24tkumoj4b` (`user_id`),
-  CONSTRAINT `FK4xc8y0nxiniot9q24tkumoj4b` FOREIGN KEY (`user_id`) REFERENCES `basic_user` (`id`)
+  KEY `FK4xc8y0nxiniot9q24tkumoj4b` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
@@ -136,10 +148,23 @@ CREATE TABLE `sys_module` (
   `expand` char(1) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKdqnac8hs89i4rxhotct6er9lr` (`create_user_id`),
-  KEY `FKad2337ra42mi99p00ctyefac8` (`mod_user_id`),
-  CONSTRAINT `FKad2337ra42mi99p00ctyefac8` FOREIGN KEY (`mod_user_id`) REFERENCES `basic_user` (`id`),
-  CONSTRAINT `FKdqnac8hs89i4rxhotct6er9lr` FOREIGN KEY (`create_user_id`) REFERENCES `basic_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  KEY `FKad2337ra42mi99p00ctyefac8` (`mod_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Records of sys_module
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_module` VALUES (1, NULL, NULL, '系统管理', 10, NULL, NULL, NULL, NULL, NULL, NULL, 'anticon anticon-setting', NULL);
+INSERT INTO `sys_module` VALUES (2, NULL, NULL, '用户管理', 1, NULL, NULL, NULL, '/sys/user', 1, NULL, 'anticon anticon-team', NULL);
+INSERT INTO `sys_module` VALUES (4, NULL, NULL, '角色管理', 1, NULL, NULL, NULL, '/sys/role', 3, NULL, 'anticon anticon-form', NULL);
+INSERT INTO `sys_module` VALUES (10, NULL, NULL, '所有菜单', NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL);
+INSERT INTO `sys_module` VALUES (12, NULL, NULL, '模块管理', 1, NULL, NULL, NULL, '/sys/module', 2, NULL, 'anticon anticon-bars', NULL);
+INSERT INTO `sys_module` VALUES (21, NULL, NULL, '字典管理', 1, NULL, NULL, NULL, '/sys/dict', 4, NULL, NULL, NULL);
+INSERT INTO `sys_module` VALUES (26, NULL, NULL, '测试模块', 10, NULL, NULL, NULL, NULL, 2, NULL, 'anticon anticon-setting', NULL);
+INSERT INTO `sys_module` VALUES (27, NULL, NULL, '测试1', 26, NULL, NULL, NULL, '/test/test1', 1, NULL, NULL, NULL);
+INSERT INTO `sys_module` VALUES (28, NULL, NULL, '测试2', 26, NULL, NULL, NULL, '/test/test2', 2, NULL, NULL, NULL);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for sys_role
@@ -155,10 +180,16 @@ CREATE TABLE `sys_role` (
   `protect` char(1) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '保留角色：1是 ',
   PRIMARY KEY (`id`),
   KEY `FKmq56blie4bjdlb0gqm3nfuote` (`create_user_id`),
-  KEY `FKoc6pejs2by1vp3041sd045rek` (`mod_user_id`),
-  CONSTRAINT `FKmq56blie4bjdlb0gqm3nfuote` FOREIGN KEY (`create_user_id`) REFERENCES `basic_user` (`id`),
-  CONSTRAINT `FKoc6pejs2by1vp3041sd045rek` FOREIGN KEY (`mod_user_id`) REFERENCES `basic_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  KEY `FKoc6pejs2by1vp3041sd045rek` (`mod_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_role` VALUES (1, '2018-09-05 16:45:30', '2019-03-07 15:11:29', '普通用户', NULL, NULL, '');
+INSERT INTO `sys_role` VALUES (2, '2018-09-09 14:03:34', '2019-03-07 15:11:32', '管理员', NULL, NULL, NULL);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for sys_role_module
@@ -171,9 +202,19 @@ CREATE TABLE `sys_role_module` (
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKjqcbph1hwbmvesk9jyg6vqcw6` (`module_id`),
-  KEY `FKejwxxhh6d84nq0fw06chviiqu` (`role_id`),
-  CONSTRAINT `FKejwxxhh6d84nq0fw06chviiqu` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`),
-  CONSTRAINT `FKjqcbph1hwbmvesk9jyg6vqcw6` FOREIGN KEY (`module_id`) REFERENCES `sys_module` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  KEY `FKejwxxhh6d84nq0fw06chviiqu` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- ----------------------------
+-- Records of sys_role_module
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_role_module` VALUES (81, NULL, 27, 1);
+INSERT INTO `sys_role_module` VALUES (82, NULL, 28, 1);
+INSERT INTO `sys_role_module` VALUES (83, NULL, 2, 2);
+INSERT INTO `sys_role_module` VALUES (84, NULL, 4, 2);
+INSERT INTO `sys_role_module` VALUES (85, NULL, 12, 2);
+INSERT INTO `sys_role_module` VALUES (86, NULL, 21, 2);
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
