8fb2b452b7e4d0ffe78c824b967af99ab92a5fec
duqian <duqian42707@163.com>
2024-02-05 18:02:21 +0800

    feat(all): 增加配置项

--- soccer-server/src/main/java/com/dqv5/soccer/common/ConfigValue.java
+++ soccer-server/src/main/java/com/dqv5/soccer/common/ConfigValue.java
@@ -12,7 +12,8 @@ import lombok.Getter;
 public enum ConfigValue {
     SYS_NAME("sys_name", "系统名称", "Soccer"),
     SYS_DESC("sys_desc", "系统描述", "Springboot + Angular 开发框架"),
-    SYS_LOGO("sys_logo", "系统Logo", "https://image.dqv5.com/public/soccer.png"),
+    SYS_LOGO_BIG("sys_logo_big", "系统Logo（大）", "https://image.dqv5.com/public/soccer.png"),
+    SYS_LOGO_SMALL("sys_logo_small", "系统Logo（小）", "https://image.dqv5.com/public/soccer.png"),
     ;
 
     private final String configKey;
