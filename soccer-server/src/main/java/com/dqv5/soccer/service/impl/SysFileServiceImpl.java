22a473f82d98c4da6939245ebbb0f69e3a2e4257
duqian <duqian42707@163.com>
2024-02-22 15:53:27 +0800

    feat(all): 文件-上传，计算sha256值

--- soccer-server/src/main/java/com/dqv5/soccer/service/impl/SysFileServiceImpl.java
+++ soccer-server/src/main/java/com/dqv5/soccer/service/impl/SysFileServiceImpl.java
@@ -1,8 +1,5 @@
 package com.dqv5.soccer.service.impl;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.dqv5.soccer.file.AmazonS3Properties;
 import com.dqv5.soccer.exception.CommonRuntimeException;
 import com.dqv5.soccer.file.FileStoreType;
 import com.dqv5.soccer.file.IntegrationFileHandler;
@@ -10,8 +7,8 @@ import com.dqv5.soccer.mapper.SysFileMapper;
 import com.dqv5.soccer.pojo.SysFile;
 import com.dqv5.soccer.service.SysFileService;
 import com.dqv5.soccer.table.SysFileTable;
-import com.dqv5.soccer.utils.SpringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,11 +23,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * @author duqian
@@ -52,16 +46,27 @@ public class SysFileServiceImpl implements SysFileService {
         String fileName = file.getOriginalFilename();
         int storeTypeValue = FileStoreType.fromName(defaultStoreType).getValue();
         IntegrationFileHandler fileHandler = this.getFileHandler(storeTypeValue);
+
+        String sha256;
         try (InputStream inputStream = file.getInputStream()) {
-            String storeInfo = fileHandler.upload(inputStream, fileName);
-            sysFileTable.setFileName(fileName);
-            sysFileTable.setStoreType(storeTypeValue);
-            sysFileTable.setStoreInfo(storeInfo);
-            sysFileTable.setFileSize(file.getSize());
-            sysFileMapper.insert(sysFileTable);
+            sha256 = DigestUtils.sha256Hex(inputStream);
+        } catch (IOException e) {
+            throw new CommonRuntimeException(e);
+        }
+
+        String storeInfo;
+        try (InputStream inputStream = file.getInputStream()) {
+            storeInfo = fileHandler.upload(inputStream, fileName);
         } catch (Exception e) {
             throw new CommonRuntimeException(e);
         }
+
+        sysFileTable.setFileName(fileName);
+        sysFileTable.setStoreType(storeTypeValue);
+        sysFileTable.setStoreInfo(storeInfo);
+        sysFileTable.setFileSize(file.getSize());
+        sysFileTable.setSha256(sha256);
+        sysFileMapper.insert(sysFileTable);
         return SysFile.of(sysFileTable);
     }
 
